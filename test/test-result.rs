b45d358ace3adcb30c62045ccada6ed5bf280019
yuanpeng <yingbo.com@gmail.com>
2022-11-02 18:57:13 +0800

    list println ?

--- test/test-result.rs
+++ test/test-result.rs
@@ -1,3 +1,7 @@
+// implement struct println with fmt::Display
+// implement List println with fmt::Display
+// Derive(Debug) for {:?}
+// return Result
 use std::process;
 use std::fmt;
 
@@ -22,10 +26,30 @@ impl fmt::Display for Config {
     }
 }
 
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let vect = &self.0;
+        let vect_iter = vect.iter();
+        write!(f, "[")?;
+        for (count, val) in vect_iter.enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}:{}", count, val)?;
+        }
+        write!(f, "]")
+    }
+}
+
+
 fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+    
     let config:Config = Config::new("hello".to_string(), "world.txt".to_string())
         .unwrap_or_else(|err| {println!("{:?}", err); process::exit(1);});
     println!("config={config}");
+    println!("config={:?}", config);
 
     let x: Result<i32, &str> = Err("hi error");
     if x.is_err() {
